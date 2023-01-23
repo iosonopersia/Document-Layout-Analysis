1524511847d84b8b86f0c04925ac07f57ed3e51e
Simone Persiani <iosonopersia@gmail.com>
2023-01-23 15:06:03 +0100

    Add colour normalization to visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -39,34 +39,50 @@ LIGHT_RED = (255, 160, 122)
 LIGHT_YELLOW = (255, 255, 224)
 
 
-def visualize_bbox(img: ndarray, bbox: COCOBoundingBox, class_name: str, colour: tuple[int]=RED, thickness: int=2) -> ndarray:
+def normalize_colour(colour: tuple[int]) -> tuple[float]:
+    """Normalizes the colour values to be between 0 and 1."""
+    return tuple([value / 255.0 for value in colour])
+
+
+def visualize_bbox(img: ndarray, bbox: COCOBoundingBox, class_name: str, border_colour: tuple[int]=RED,
+                   thickness: int=2, is_normalized: bool=True) -> ndarray:
     """Visualizes a single bounding box on the image"""
     x_min, y_min, x_max, y_max = int(bbox.x1), int(bbox.y1), int(bbox.x2), int(bbox.y2)
     assert x_min < x_max and y_min < y_max, "Bounding box is not valid."
 
-    cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=colour, thickness=thickness)
+    bg_colour = WHITE
+    text_colour = BLACK
+    if is_normalized:
+        border_colour = normalize_colour(border_colour)
+        bg_colour = normalize_colour(bg_colour)
+        text_colour = normalize_colour(text_colour)
 
+    # Draw the bounding box on the image
+    cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=border_colour, thickness=thickness)
+
+    # Display the label at the top of the bounding box
     ((text_width, text_height), _) = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)
-    cv2.rectangle(img, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), WHITE, -1)
+    cv2.rectangle(img, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), bg_colour, -1)
     cv2.putText(
         img,
         text=class_name,
         org=(x_min, y_min - int(0.3 * text_height)),
         fontFace=cv2.FONT_HERSHEY_SIMPLEX,
         fontScale=0.35,
-        color=BLACK,
+        color=text_colour,
         lineType=cv2.LINE_AA,
     )
     return img
 
 
 def visualize(image: ndarray, bboxes: list[COCOBoundingBox], category_ids: list[int],
-              category_id_to_name: dict[int, str], category_id_to_colour: dict[int, tuple[int]]) -> None:
+              category_id_to_name: dict[int, str], category_id_to_colour: dict[int, tuple[int]],
+              is_normalized: bool=True) -> None:
     img = image.copy()
     for bbox, category_id in zip(bboxes, category_ids):
         class_name = category_id_to_name[category_id]
         colour = category_id_to_colour[category_id]
-        img = visualize_bbox(img, bbox, class_name, colour)
+        img = visualize_bbox(img, bbox, class_name, colour, is_normalized=is_normalized)
     plt.figure(figsize=(12, 12))
     plt.axis('off')
     plt.imshow(img)
