3b244935669687c8ec16afcc2b34af9003ba0543
Simone Persiani <iosonopersia@gmail.com>
2023-02-03 10:17:20 +0100

    Reduce memory consumption for get_mean_std.py

--- src/get_mean_std.py
+++ src/get_mean_std.py
@@ -26,19 +26,33 @@ if __name__ == '__main__':
 
     print("Loading training images to compute mean and std across each color channel...")
     image_folder = os.path.abspath(config.dataset.images_dir)
-    images = []
-    for image_filename in tqdm(images_set):
+    image_mean = np.zeros(3, dtype=np.float32)
+    image_std = np.zeros(3, dtype=np.float32)
+
+    loop = tqdm(images_set, leave=True)
+    loop.set_description(f"Mean and std of training images")
+    loop.set_postfix(mean=0.0, std=0.0)
+
+    num_processed_images = 0
+    for image_filename in loop:
         image_path = os.path.join(image_folder, image_filename)
 
         image = Image.open(image_path).convert('RGB')
-        image = np.asarray(image, dtype=np.float32)
+        image = np.asarray(image, dtype=np.float32) # (H, W, C)
         image = image / 255.0
 
-        images.append(image)
+        image_mean += np.mean(image, axis=(0, 1))
+        image_std += np.std(image, axis=(0, 1))
 
-    images = np.stack(images, axis=0) # (N, H, W, C)
-    mean_per_channel = np.mean(images, axis=(0, 1, 2))
-    std_per_channel = np.std(images, axis=(0, 1, 2))
+        num_processed_images += 1
+
+        # Update progress bar
+        mean_to_display = (image_mean / num_processed_images).mean()
+        std_to_display = (image_std / num_processed_images).mean()
+        loop.set_postfix(mean=mean_to_display, std=std_to_display)
+
+    mean_per_channel = image_mean / len(images_set)
+    std_per_channel = image_std / len(images_set)
 
     print(f'Mean: R={mean_per_channel[0]} G={mean_per_channel[1]} B={mean_per_channel[2]}')
     print(f'Std: R={std_per_channel[0]} G={std_per_channel[1]} B={std_per_channel[2]}')
