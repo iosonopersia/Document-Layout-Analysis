bfb0e01f3464cba9347099003beef911457d7f96
Simone Persiani <iosonopersia@gmail.com>
2023-02-04 13:37:20 +0100

    Update test.py

--- src/test.py
+++ src/test.py
@@ -21,9 +21,9 @@ def test_metrics():
     pred_boxes, true_boxes = get_evaluation_bboxes(
         eval_loader,
         model,
-        iou_threshold=test_cfg.nms_iou_threshold,
-        anchors=ANCHORS_DICT,
-        confidence_threshold=test_cfg.confidence_threshold,
+        iou_threshold=test_cfg.non_max_suppression.min_iou,
+        anchors=SCALED_ANCHORS,
+        confidence_threshold=test_cfg.non_max_suppression.min_confidence,
     )
     print("Computing mAP...")
     mAP_values = mean_average_precision(
@@ -31,14 +31,15 @@ def test_metrics():
         true_boxes,
         num_classes=NUM_CLASSES,
     )
-    iou_thresholds = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+    iou_thresholds = ["0.50", "0.55", "0.60", "0.65", "0.70", "0.75", "0.80", "0.85", "0.90", "0.95"]
     for c in range(NUM_CLASSES):
-        mAP_per_class = mAP_values[:, c].mean().item()
-        print(f"mAP@0.5:0.95 for class {c+1}: {round(100*mAP_per_class, 3)} %")
+        mAP_per_class = mAP_values[:, c].mean().item() * 100.0
+        print("="*50)
+        print(f"[CLASS {c+1}] mAP@0.50:0.95 {round(mAP_per_class, 3)} %")
+        print("="*50)
         for i, iou in enumerate(iou_thresholds):
-            mAP_per_class_per_iou = mAP_values[i, c].item()
-            print(f"\tmAP@{iou} for class {c+1}: {round(100*mAP_per_class_per_iou, 3)} %")
-
+            mAP_per_class_per_iou = mAP_values[i, c].item() * 100.0
+            print(f"\tmAP@{iou}\t{round(mAP_per_class_per_iou, 3)} %")
 
 
 if __name__ == "__main__":
@@ -52,7 +53,7 @@ if __name__ == "__main__":
     model_cfg = config.model
     test_cfg = config.test
 
-    # ============TOOLS==============
+    # =============TOOLS==============
     checkpoint_handler = CheckpointHandler(config.checkpoint)
 
     # ===========DATASET==============
