e329f8cc50d1737c54b1b63dbfe22fcb06899f45
Simone Persiani <iosonopersia@gmail.com>
2023-02-04 13:39:03 +0100

    Fix and cleanup bboxes_extraction.py

--- src/metrics/bboxes_extraction.py
+++ src/metrics/bboxes_extraction.py
@@ -59,32 +59,37 @@ def get_evaluation_bboxes(
 
 
 def predictions_to_bboxes(predictions: dict[int, torch.Tensor], image_ids: torch.Tensor, anchors: dict[int, torch.Tensor]) -> torch.Tensor:
+    OBJ_SCORE = [0]
+    BOX_COORDS = [1, 2, 3, 4]
+    BOX_POS = [0, 1]
+    BOX_SIZE = [2, 3]
+
     bboxes = []
     for S in predictions.keys():
-        scaled_anchors = anchors[S] * S
         B = predictions[S].shape[0]
+        C = predictions[S].shape[-1] - 5
+        CLASSES = list(range(5, 5 + C))
 
-        pred_box = predictions[S][..., 1:5] # (B, 3, S, S, 4)
-        pred_score = torch.sigmoid(predictions[S][..., 0:1]) # (B, 3, S, S, 1)
-        pred_class = torch.argmax(predictions[S][..., 5:], dim=-1, keepdim=True) # (B, 3, S, S, 1)
+        pred_score = torch.sigmoid(predictions[S][..., OBJ_SCORE]) # (B, 3, S, S, 1)
+        pred_class = torch.argmax(predictions[S][..., CLASSES], dim=-1, keepdim=True) # (B, 3, S, S, 1)
         pred_image_id = image_ids.reshape(B, 1, 1, 1, 1).repeat(1, 3, S, S, 1) # (B, 3, S, S, 1)
 
         # ===================================================
         # Apply required transformations to model predictions
         # ===================================================
-        scaled_anchors = scaled_anchors.reshape(1, 3, 1, 1, 2)
-        pred_box[..., 0:2] = torch.sigmoid(pred_box[..., 0:2])
-        pred_box[..., 2:4] = torch.exp(pred_box[..., 2:4]) * scaled_anchors
+        pred_box = predictions[S][..., BOX_COORDS] # (B, 3, S, S, 4)
+        pred_box[..., BOX_POS] = torch.sigmoid(pred_box[..., BOX_POS])
+        pred_box[..., BOX_SIZE] = torch.exp(pred_box[..., BOX_SIZE]) * anchors[S].reshape(1, 3, 1, 1, 2)
 
         # =============================================
         # Convert box positions from (dx, dy) to (x, y)
         # =============================================
-        cell_indices = torch.arange(S, dtype=torch.float32)
+        cell_indices = torch.arange(S, dtype=torch.int16)
         cell_i = cell_indices.reshape(1, 1, S, 1, 1).repeat(B, 3, 1, S, 1)
         cell_j = cell_indices.reshape(1, 1, 1, S, 1).repeat(B, 3, S, 1, 1)
-        cell_indices = torch.cat((cell_i, cell_j), dim=-1)
+        cell_indices = torch.cat((cell_j, cell_i), dim=-1)
 
-        pred_box[..., 0:2] += cell_indices
+        pred_box[..., BOX_POS] += cell_indices.float()
 
         # ================================================
         # Scale box coordinates to the original image size
@@ -99,38 +104,46 @@ def predictions_to_bboxes(predictions: dict[int, torch.Tensor], image_ids: torch
 
 
 def targets_to_bboxes(targets: dict[int, torch.Tensor], image_ids: torch.Tensor) -> torch.Tensor:
-    OBJ_SCORE = [0]
     # We can consider just one scale, since bboxes are the same across all scales
     # We choose the last scale, which is the smallest one, to speed up the process
     S = list(targets.keys())[-1]
     B = targets[S].shape[0]
     C = targets[S].shape[-1] - 5
 
+    OBJ_SCORE = [0]
+    BOX_COORDS = [1, 2, 3, 4]
+    BOX_POS = [0, 1]
+    # BOX_SIZE = [2, 3]
+    CLASSES = list(range(5, 5 + C))
+
     true_boxes_per_image = []
     for i in range(B):
-        tgt_per_image = targets[S][[i], ...] # (1, 3, S, S, 16)
+        tgt_per_image = targets[S][i, ...] # (3, S, S, 16)
 
         # ================================================
         # Filter out non-object bounding boxes from target
         # ================================================
-        obj_mask = tgt_per_image[..., OBJ_SCORE] == 1
-        tgt = tgt_per_image[obj_mask.repeat(1, 1, 1, 1, 5 + C)].reshape(-1, 5 + C) # (N, 5+C)
+        obj_mask = tgt_per_image[..., OBJ_SCORE] == 1 # (3, S, S, 1)
+        tgt = tgt_per_image[obj_mask.repeat(1, 1, 1, 5 + C)].reshape(-1, 5 + C) # (N, 5+C)
         N = tgt.shape[0]
-        tgt_class = torch.argmax(tgt[:, 5:16], dim=-1, keepdim=True) # (N, 1)
+
         tgt_image_id = image_ids[[i]].repeat(N, 1) # (N, 1)
+        tgt_class = torch.argmax(tgt[:, CLASSES], dim=-1, keepdim=True) # (N, 1)
+        tgt_score = torch.ones((N, 1), dtype=torch.float32) # (N, 1)
+        tgt_boxes = tgt[:, BOX_COORDS] # (N, 4)
 
         # =============================================
         # Convert box positions from (dx, dy) to (x, y)
         # =============================================
-        cell_indices = torch.argwhere(obj_mask)[:, [3, 2]] # (j, i) for each bbox (N, 2)
-        tgt[:, [1, 2]] += cell_indices
+        cell_indices = torch.argwhere(obj_mask)[:, [2, 1]] # (j, i) for each bbox (N, 2)
+        tgt_boxes[:, BOX_POS] += cell_indices
 
         # ================================================
         # Scale box coordinates to the original image size
         # ================================================
-        tgt[:, 1:5] *= (1025 / S)
+        tgt_boxes *= (1025 / S)
 
-        tgt = torch.cat([tgt_image_id, tgt_class, tgt[:, 0:5]], dim=-1) # (N, 1) + (N, 1) + (N, 5) -> (N, 7)
+        tgt = torch.cat([tgt_image_id, tgt_class, tgt_score, tgt_boxes], dim=-1) # (N, 1) + (N, 1) + (N, 1) + (N, 4) -> (N, 7)
         true_boxes_per_image.append(tgt)
 
     true_boxes = torch.cat(true_boxes_per_image, dim=0) # (#GT_BOXES, 7)
