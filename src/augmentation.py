681828c8f2d7e1cc9ffa4330c13c6324782f62cd
Simone Persiani <iosonopersia@gmail.com>
2023-02-05 15:57:38 +0100

    Use the same augmentation scheme as DETR (and DiT)

--- src/augmentation.py
+++ src/augmentation.py
@@ -8,34 +8,48 @@ SCALE = 1.2
 
 train_transforms = A.Compose(
     [
-        A.LongestMaxSize(max_size=int(IMAGE_SIZE * SCALE)),
-        A.PadIfNeeded(
-            min_height=int(IMAGE_SIZE * SCALE),
-            min_width=int(IMAGE_SIZE * SCALE),
-            border_mode=cv2.BORDER_CONSTANT,
-        ),
-        A.RandomCrop(width=IMAGE_SIZE, height=IMAGE_SIZE),
-        A.OneOf(
-            [
-                A.Affine(
-                    scale=(0.8, 1.2),
-                    translate_percent={'x': (-0.1, 0.1), 'y': (-0.1, 0.1)},
-                    rotate=(-5, 5),
-                    shear=(-10, 10),
-                    p=0.5,
-                    mode=cv2.BORDER_CONSTANT,
-                    cval=(255, 255, 255)),
-                A.Perspective(
-                    scale=(0.05, 0.1),
-                    p=0.5,
-                    pad_mode=cv2.BORDER_CONSTANT,
-                    pad_val=(255, 255, 255)),
-            ],
-            p=0.8),
-        A.ToGray(p=0.2),
+        A.OneOf([
+            # Just resize the image
+            A.Compose([
+                A.LongestMaxSize(max_size=IMAGE_SIZE),
+                A.PadIfNeeded(
+                    min_height=IMAGE_SIZE,
+                    min_width=IMAGE_SIZE,
+                    border_mode=cv2.BORDER_CONSTANT,
+                ),
+            ], p=0.5),
+            # Resize the image and crop a random part of it
+            A.Compose([
+                A.LongestMaxSize(max_size=int(IMAGE_SIZE * SCALE)),
+                A.PadIfNeeded(
+                    min_height=int(IMAGE_SIZE * SCALE),
+                    min_width=int(IMAGE_SIZE * SCALE),
+                    border_mode=cv2.BORDER_CONSTANT,
+                ),
+                A.RandomCrop(width=IMAGE_SIZE, height=IMAGE_SIZE),
+            ], p=0.5),
+        ], p=1.0),
+
+        # A.OneOf(
+        #     [
+        #         A.Affine(
+        #             scale=(0.8, 1.2),
+        #             translate_percent={'x': (-0.1, 0.1), 'y': (-0.1, 0.1)},
+        #             rotate=(-5, 5),
+        #             shear=(-10, 10),
+        #             p=0.5,
+        #             mode=cv2.BORDER_CONSTANT,
+        #             cval=(255, 255, 255)),
+        #         A.Perspective(
+        #             scale=(0.05, 0.1),
+        #             p=0.5,
+        #             pad_mode=cv2.BORDER_CONSTANT,
+        #             pad_val=(255, 255, 255)),
+        #     ],
+        #     p=0.8),
+        # A.ToGray(p=0.5),
     ],
     bbox_params=A.BboxParams(format='coco', min_visibility=0.3, label_fields=['category_ids']),
-    p=0.3, # ~30% of the images will be transformed
 )
 
 test_transforms = A.Compose(
