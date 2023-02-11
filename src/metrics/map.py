2684acf811ff07ee867894df04a4b8bc46a82901
Simone Persiani <iosonopersia@gmail.com>
2023-02-11 18:58:13 +0100

    Vectorize mAP computation over each IoU threshold

--- src/metrics/map.py
+++ src/metrics/map.py
@@ -1,25 +1,35 @@
+from matplotlib import pyplot as plt
 import torch
 from tqdm import tqdm
 from metrics.iou import intersection_over_union
 
 
-def mean_average_precision(pred_boxes: torch.Tensor, true_boxes: torch.Tensor, num_classes: int=11):
-    iou_thresholds = torch.arange(0.5, 1.0, 0.05, dtype=torch.float32)
+def mean_average_precision(pred_boxes: torch.Tensor, true_boxes: torch.Tensor, iou_thresholds: torch.Tensor, num_classes: int=11):
+    num_iou_thresholds = iou_thresholds.shape[0]
+    if num_iou_thresholds < 1:
+        raise ValueError(f"Number of IoU thresholds must be greater than 0, got {num_iou_thresholds}")
 
-    mAP_per_iou_per_class = torch.zeros((iou_thresholds.numel(), num_classes), dtype=torch.float32)
-    for i in tqdm(range(iou_thresholds.numel())):
-        iou_threshold = iou_thresholds[i].item()
-        for c in range(num_classes):
-            mAP_per_iou_per_class[i, c] = average_precision_per_class(pred_boxes, true_boxes, iou_threshold, c)
+    mAP_per_class = torch.zeros((num_classes, num_iou_thresholds), dtype=torch.float32)
 
-    return mAP_per_iou_per_class
+    loop = tqdm(range(num_classes), leave=True)
+    loop.set_description(f"COCOmAP per class")
+
+    for c in loop:
+        mAP_per_class[c, :] = average_precision_per_class(pred_boxes, true_boxes, iou_thresholds, c)
+        loop.set_postfix(class_id=c+1)
+
+    return mAP_per_class
 
 
-def average_precision_per_class(pred_boxes: torch.Tensor, true_boxes: torch.Tensor, iou_threshold: float, class_id: int) -> float:
+def average_precision_per_class(pred_boxes: torch.Tensor, true_boxes: torch.Tensor, iou_thresholds: torch.Tensor, class_id: int) -> float:
     SAMPLE_ID = [0]
     CLASS_ID = [1]
     OBJ_SCORE = [2]
     BBOX = [3, 4, 5, 6]
+
+    num_iou_thresholds = iou_thresholds.shape[0]
+    if num_iou_thresholds < 1:
+        raise ValueError(f"Number of IoU thresholds must be greater than 0, got {num_iou_thresholds}")
 
     detections_mask = (pred_boxes[:, CLASS_ID] == class_id).repeat(1, 7)
     detections = pred_boxes[detections_mask].reshape(-1, 7)
@@ -34,11 +44,14 @@ def average_precision_per_class(pred_boxes: torch.Tensor, true_boxes: torch.Tens
         return 0.0
 
     img_ids, num_bboxes_per_img = ground_truths[:, SAMPLE_ID].int().unique(sorted=False, return_counts=True, dim=None)
-    is_matched_bbox = {img_ids[i].item(): torch.zeros(num_bboxes_per_img[i], dtype=torch.bool) for i in range(img_ids.shape[0])}
+    is_matched_bbox = {
+        img_ids[i].item(): torch.zeros((num_bboxes_per_img[i], num_iou_thresholds), dtype=torch.bool)
+        for i in range(img_ids.shape[0])
+    }
+    is_TP = torch.zeros((num_detections, num_iou_thresholds), dtype=torch.bool)
 
     sorted_indices = detections[:, OBJ_SCORE].flatten().argsort(stable=False, descending=True, dim=0)
     detections = detections[sorted_indices, :]
-    is_TP = torch.zeros(num_detections, dtype=torch.bool)
 
     for i in range(num_detections):
         detection = detections[i, :]
@@ -57,28 +70,41 @@ def average_precision_per_class(pred_boxes: torch.Tensor, true_boxes: torch.Tens
 
         ious = intersection_over_union(detection[BBOX], ground_truth_img[:, BBOX]) # (num_ground_truths, 1)
         best_gt_idx = torch.argmax(ious, dim=0)
-        best_iou = ious[best_gt_idx]
+        best_iou = ious[best_gt_idx] # (1,)
 
-        if best_iou > iou_threshold and not is_matched_bbox[detection_img_id][best_gt_idx]:
-            is_TP[i] = True
-            is_matched_bbox[detection_img_id][best_gt_idx] = True
+        is_TP_mask = best_iou > iou_thresholds # (num_iou_thresholds,)
+        is_not_matched = torch.logical_not(is_matched_bbox[detection_img_id][best_gt_idx, :]) # (num_iou_thresholds,)
+
+        is_TP[i, :] = torch.logical_and(is_TP_mask, is_not_matched)
+        is_matched_bbox[detection_img_id][best_gt_idx, :] = torch.logical_or(is_TP_mask, is_matched_bbox[detection_img_id][best_gt_idx, :])
 
     # Compute precision and recall at each detection threshold
-    if is_TP.sum() == 0:
-        # AP is zero if there are no true positives
-        return 0.0
-
     TP_cumsum = torch.cumsum(is_TP, dim=0)
-    precisions = TP_cumsum / torch.arange(1, num_detections + 1, dtype=torch.float32)
+    precisions = torch.div(TP_cumsum, torch.arange(1, num_detections + 1, dtype=torch.float32).unsqueeze(dim=-1))
     recalls = TP_cumsum / num_ground_truths
 
-    # Add first and last points to the ROC curve
-    ZERO = torch.zeros(1, dtype=torch.float32)
-    ONE = torch.ones(1, dtype=torch.float32)
-    # First point is (0, 1) and last point is (1, 0)
-    recalls = torch.cat([ZERO, recalls, ONE], dim=0)
-    precisions = torch.cat([ONE, precisions, ZERO], dim=0)
+    ZERO = torch.zeros((1, num_iou_thresholds), dtype=torch.float32)
+    ONE = torch.ones((1, num_iou_thresholds), dtype=torch.float32)
+    # First point is always (0, 1)
+    recalls = torch.cat([ZERO, recalls], dim=0)
+    precisions = torch.cat([ONE, precisions], dim=0)
+
+    # Save the plot figure of the precision-recall curve
+    save_precision_recall_plot(precisions, recalls, iou_thresholds, class_id)
 
     # Numerical integration of the precision-recall curve
     average_precision = torch.trapezoid(y=precisions, x=recalls, dim=0)
     return average_precision
+
+
+def save_precision_recall_plot(precisions: torch.Tensor, recalls: torch.Tensor, iou_thresholds: torch.Tensor, class_id: int) -> None:
+    fig = plt.figure()
+    plt.plot(recalls, precisions)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.title(f"Precision-Recall curves for class {class_id + 1}")
+    plt.legend([f"IoU {iou:.2f}" for iou in iou_thresholds])
+    plt.savefig(f"./data/mAP_plots/mAP-class-{class_id + 1}.png")
+    plt.close(fig)
--- src/test.py
+++ src/test.py
@@ -1,3 +1,4 @@
+import pickle
 from collections import OrderedDict
 
 import torch
@@ -28,24 +29,43 @@ def get_bboxes_from_test_set():
         anchors=SCALED_ANCHORS,
         confidence_threshold=test_cfg.non_max_suppression.min_confidence,
     )
-    return pred_boxes, true_boxes
+    with open("data/pred_boxes.pkl", "wb") as f:
+        pickle.dump(pred_boxes, f)
+    with open("data/true_boxes.pkl", "wb") as f:
+        pickle.dump(true_boxes, f)
 
 
 def test_metrics():
-    pred_boxes, true_boxes = get_bboxes_from_test_set()
+    with open("data/pred_boxes.pkl", "rb") as f:
+        pred_boxes = pickle.load(f)
+    with open("data/true_boxes.pkl", "rb") as f:
+        true_boxes = pickle.load(f)
+
     print("Computing mAP...")
+    iou_thresholds = torch.tensor([0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95], dtype=torch.float32)
     mAP_values = mean_average_precision(
         pred_boxes,
         true_boxes,
-        num_classes=NUM_CLASSES,
+        iou_thresholds,
+        NUM_CLASSES,
     )
-    iou_thresholds = ["0.50", "0.55", "0.60", "0.65", "0.70", "0.75", "0.80", "0.85", "0.90", "0.95"]
+
     for c in range(NUM_CLASSES):
-        mAP_per_class = mAP_values[:, c].mean().item() * 100.0
+        mAP_per_class = mAP_values[c, :].mean().item() * 100.0
         print("="*50)
         print(f"[CLASS {c+1}] mAP@0.50:0.95 {round(mAP_per_class, 3)} %")
         print("="*50)
         for i, iou in enumerate(iou_thresholds):
+            mAP_per_class_per_iou = mAP_values[c, i].item() * 100.0
+            print(f"\tmAP@{iou:.2f}\t{round(mAP_per_class_per_iou, 3)} %")
+
+    mAP = mAP_values.mean().item() * 100.0
+    print("="*50)
+    print(f"[OVERALL] mAP@0.5:0.95 {round(mAP, 3)} %")
+    print("="*50)
+    for i, iou in enumerate(iou_thresholds):
+        mAP_per_iou = mAP_values[:, i].mean().item() * 100.0
+        print(f"\tmAP@{iou:.2f}\t{round(mAP_per_iou, 3)} %")
             mAP_per_class_per_iou = mAP_values[i, c].item() * 100.0
             print(f"\tmAP@{iou}\t{round(mAP_per_class_per_iou, 3)} %")
 
@@ -72,4 +92,5 @@ if __name__ == "__main__":
         for size, anchors in ANCHORS_DICT.items()
     })
 
+    get_bboxes_from_test_set()
     test_metrics()
